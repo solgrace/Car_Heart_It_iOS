bcd8035d8c746ab1d53efcc345887419f99feb58
solgrace <solibungrace@gmail.com>
2023-09-27 20:06:30 +1000

    Successfully located and rendered default user location on map. I have also learned that Simulators are capable of simulating location data, but by default, they do not have access to your real-world location. To test location-related functionality in a simulator and simulate your current location, you need to manually set a location for the simulator. Hence why when I run my application, it renders the default location which is located in United States, California, Silicon Valley. For now, my location will be set to the default location.

--- Car_Heart_It_iOS/View/CarRentalMapView.swift
+++ Car_Heart_It_iOS/View/CarRentalMapView.swift
@@ -5,51 +5,39 @@
 //  Created by Grace Rufina Solibun on 26/9/2023.
 //
 
-//import SwiftUI
-//
-//struct CarRentalMapView: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-//}
-//
-//struct CarRentalMapView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CarRentalMapView()
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
 import SwiftUI
+import MapboxMaps
+import CoreLocation
 
+// CORRECT VERSION. THE VERSION THAT IS CAPABLE OF LOCATING REAL-TIME LOCATION IF RUN ON A REAL DEVICE. SADLY SIMULATORS DOES NOT HAVE THIS CAPABILITY
 struct CarRentalMapView: View {
+    @StateObject var locationManager = LocationManager()
+
     var body: some View {
         NavigationView {
             VStack {
-                Text("Welcome to Car Rental App")
-                    .font(.largeTitle)
-                    .padding()
+                MapViewContainer(userLocation: $locationManager.userLocation)
+                    .onAppear {
+                        // Check the authorization status before requesting location updates
+                        if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
+                            locationManager.startUpdatingLocation()
+                        } else {
+                            // Handle other cases as needed
+                        }
+                    }
+                    .onReceive(locationManager.$userLocation) { newUserLocation in
+                        // Handle user location updates here, e.g., update the map
+                        // newUserLocation contains the updated user location
+                    }
 
-                MapViewContainer()
-                    .frame(height: 300) // Adjust the frame size as needed
-
-                // Other UI components
+                if let userLocation = locationManager.userLocation {
+                    Text("Latitude: \(userLocation.latitude), Longitude: \(userLocation.longitude)")
+                } else {
+                    Text("Waiting for location...")
+                }
             }
             .padding()
         }
-    }
-}
-
-struct CarRentalMapView_Previews: PreviewProvider {
-    static var previews: some View {
-        CarRentalMapView()
+        .navigationBarBackButtonHidden(true)
     }
 }
--- Car_Heart_It_iOS/View/MapViewContainer.swift
+++ Car_Heart_It_iOS/View/MapViewContainer.swift
@@ -5,28 +5,32 @@
 //  Created by Grace Rufina Solibun on 26/9/2023.
 //
 
-import MapboxMaps
+// CORRECT VERSION. THE VERSION THAT IS CAPABLE OF LOCATING REAL-TIME LOCATION IF RUN ON A REAL DEVICE. SADLY SIMULATORS DOES NOT HAVE THIS CAPABILITY
 import SwiftUI
+import MapboxMaps
 
-// Create a UIViewRepresentable struct
 struct MapViewContainer: UIViewRepresentable {
-    // Mapbox access token
     let accessToken = "sk.eyJ1IjoiZ3JhY2Vzb2wiLCJhIjoiY2xteng2eWF0MW5zdjJqbnRrN3dlNXVkaiJ9.xYpQiPGGe01KVlnx9Qf0_w"
+    @Binding var userLocation: CLLocationCoordinate2D?
 
     func makeUIView(context: Context) -> MapView {
-        // Create resource options with your access token
         let resourceOptions = ResourceOptions(accessToken: accessToken)
-
-        // Create map init options with the resource options
         let mapInitOptions = MapInitOptions(resourceOptions: resourceOptions)
+        let mapView = MapView(frame: UIScreen.main.bounds, mapInitOptions: mapInitOptions)
 
-        // Specify the frame for the mapView
-        let mapView = MapView(frame: CGRect(x: 0, y: 0, width: 300, height: 300), mapInitOptions: mapInitOptions)
+        // Enable user location tracking
+        mapView.location.options.puckType = .puck2D()
 
         return mapView
     }
 
     func updateUIView(_ uiView: MapView, context: Context) {
-        // Update the UIView if needed
+        // Update the map view's center coordinate when userLocation changes
+        if let userLocation = userLocation {
+            let cameraOptions = CameraOptions(center: userLocation, zoom: 15)
+            uiView.camera.ease(to: cameraOptions, duration: 1.0) { _ in
+                // Completion handler, if needed
+            }
+        }
     }
 }
