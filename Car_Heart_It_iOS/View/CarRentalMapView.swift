73726ba85ede765213d288cf7c50c0631b1cbe9c
solgrace <solibungrace@gmail.com>
2023-09-29 11:57:40 +1000

    What I worked on:
    Successfully Implemented:
    - Added data structures for handling event-related information. Created models: APIResponse, APIParameters, EventData, InfoLinkData, and VenueData to efficiently manage data retrieved from the API. These changes lay the groundwork for parsing and utilising API responses effectively.
    - Successfully implemented EventAPIClient for fetching events data near the user's location from the API. Successfully retrieved event data from the API, including InfoLinkData and VenueData.
    
    Work in progress:
    - Yet to still figure out how to render the successfully fetched locations as pinpoints on the map:
    - CarRentalMapView: Initial implementation to display events on a map and in a list view. Issues with pinpoints rendering.
    - MapViewContainer: Initial setup for displaying a Mapbox map showing pinpoints from events. Issues with pinpoints rendering.

--- Car_Heart_It_iOS/View/CarRentalMapView.swift
+++ Car_Heart_It_iOS/View/CarRentalMapView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import MapboxMaps
 import CoreLocation
+import Combine
+import _MapKit_SwiftUI
 
 //// CORRECT VERSION. THE VERSION THAT IS CAPABLE OF LOCATING REAL-TIME LOCATION IF RUN ON A REAL DEVICE. SADLY SIMULATORS DOES NOT HAVE THIS CAPABILITY
 //struct CarRentalMapView: View {
@@ -83,18 +85,125 @@ import CoreLocation
 
 
 
-// THE VERSION OF USING MAPBOX TP DETECT MY LOCATION
-struct CarRentalMapView: View {
-    // Remove @StateObject for locationManager
-    // @StateObject var locationManager = LocationManager()
+//// THE VERSION OF USING MAPBOX TP DETECT MY LOCATION
+//// CORRECT VERSION FROM BEFORE. UN-COMMENT THE BELOW ONE.
+//struct CarRentalMapView: View {
+//    // Remove @StateObject for locationManager
+//    // @StateObject var locationManager = LocationManager()
+//
+//    @ObservedObject var locationManager = LocationManager() // Use @ObservedObject
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                // Pass the userLocation binding to MapViewContainer
+//                MapViewContainer(userLocation: $locationManager.userLocation)
+//                    .onAppear {
+//                        // Check the authorization status before requesting location updates
+//                        if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
+//                            locationManager.startUpdatingLocation()
+//                        } else {
+//                            // Handle other cases as needed
+//                        }
+//                    }
+//                    .onReceive(locationManager.$userLocation) { newUserLocation in
+//                        // Handle user location updates here, e.g., update the map
+//                        // newUserLocation contains the updated user location
+//                    }
+//
+//                if let userLocation = locationManager.userLocation {
+//                    Text("Latitude: \(userLocation.latitude), Longitude: \(userLocation.longitude)")
+//                } else {
+//                    Text("Waiting for location...")
+//                }
+//            }
+//            .padding()
+//        }
+//        .navigationBarBackButtonHidden(true)
+//    }
+//}
 
-    @ObservedObject var locationManager = LocationManager() // Use @ObservedObject
+
+
+
+
+//// RENDERING EVENTS AS LISTS
+//struct CarRentalMapView: View {
+//    @ObservedObject var locationManager = LocationManager()
+//    @State private var events: [EventData] = [] // Store the retrieved events
+//    @State private var cancellables: Set<AnyCancellable> = [] // For managing Combine subscriptions
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                MapViewContainer(userLocation: $locationManager.userLocation)
+//                    .onAppear {
+//                        // Check the authorization status before requesting location updates
+//                        if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
+//                            locationManager.startUpdatingLocation()
+//                        } else {
+//                            // Handle other cases as needed
+//                        }
+//
+//                        // Fetch events near the user's location
+//                        fetchEventsNearUserLocation()
+//                    }
+//
+//                if !events.isEmpty {
+//                    List(events) { event in
+//                        Text("Event Name: \(event.name)")
+//                        // Display other event details here
+//                    }
+//                } else {
+//                    Text("No events found near your location.")
+//                }
+//            }
+//            .padding()
+//        }
+//        .navigationBarBackButtonHidden(true)
+//    }
+//
+//    private func fetchEventsNearUserLocation() {
+//        guard let userLocation = locationManager.userLocation else {
+//            print("User location not available.")
+//            return
+//        }
+//
+//        EventAPIClient().fetchEventsNearUserLocation(userLocation: userLocation)
+//            .receive(on: DispatchQueue.main) // Ensure UI updates are on the main thread
+//            .sink(receiveCompletion: { completion in
+//                switch completion {
+//                case .finished:
+//                    break // Handle the completion if needed
+//                case .failure(let error):
+//                    print("Error: \(error)")
+//                }
+//            }, receiveValue: { apiResponses in
+//                // Extract the events from the array of APIResponse objects
+//                let eventsFromAPIResponses = apiResponses.flatMap { $0.data }
+//
+//                // Append the extracted events to the events array
+//                events.append(contentsOf: eventsFromAPIResponses)
+//            })
+//            .store(in: &cancellables)
+//    }
+//
+//}
+
+
+
+
+
+// RENDERING EVENTS AS ANNOTATIONS ON MAP
+struct CarRentalMapView: View {
+    @ObservedObject var locationManager = LocationManager()
+    @State private var events: [EventData] = [] // Store the retrieved events
+    @State private var cancellables: Set<AnyCancellable> = [] // For managing Combine subscriptions
 
     var body: some View {
         NavigationView {
             VStack {
-                // Pass the userLocation binding to MapViewContainer
-                MapViewContainer(userLocation: $locationManager.userLocation)
+                MapViewContainer(userLocation: $locationManager.userLocation, annotations: createAnnotationsFromEvents(events: events))
                     .onAppear {
                         // Check the authorization status before requesting location updates
                         if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
@@ -102,20 +211,60 @@ struct CarRentalMapView: View {
                         } else {
                             // Handle other cases as needed
                         }
-                    }
-                    .onReceive(locationManager.$userLocation) { newUserLocation in
-                        // Handle user location updates here, e.g., update the map
-                        // newUserLocation contains the updated user location
+
+                        // Fetch events near the user's location
+                        fetchEventsNearUserLocation()
                     }
 
-                if let userLocation = locationManager.userLocation {
-                    Text("Latitude: \(userLocation.latitude), Longitude: \(userLocation.longitude)")
+                if !events.isEmpty {
+                    List(events) { event in
+                        Text("Event Name: \(event.name)")
+                        // Display other event details here
+                    }
                 } else {
-                    Text("Waiting for location...")
+                    Text("No events found near your location.")
                 }
             }
             .padding()
         }
         .navigationBarBackButtonHidden(true)
     }
+        
+    private func createAnnotationsFromEvents(events: [EventData]) -> some View {
+        return ZStack {
+            ForEach(events, id: \.id) { event in
+                if let latitude = event.venue?.latitude,
+                   let longitude = event.venue?.longitude {
+                    MapViewAnnotation(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), title: event.name)
+                        .offset(x: 0, y: -15) // Adjust the offset as needed
+                }
+            }
+        }
+    }
+
+    private func fetchEventsNearUserLocation() {
+        guard let userLocation = locationManager.userLocation else {
+            print("User location not available.")
+            return
+        }
+
+        EventAPIClient().fetchEventsNearUserLocation(userLocation: userLocation)
+            .receive(on: DispatchQueue.main) // Ensure UI updates are on the main thread
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break // Handle the completion if needed
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            }, receiveValue: { apiResponses in
+                // Extract the events from the array of APIResponse objects
+                let eventsFromAPIResponses = apiResponses.flatMap { $0.data }
+
+                // Append the extracted events to the events array
+                events.append(contentsOf: eventsFromAPIResponses)
+            })
+            .store(in: &cancellables)
+    }
+
 }
--- Car_Heart_It_iOS/View/MapViewContainer.swift
+++ Car_Heart_It_iOS/View/MapViewContainer.swift
@@ -74,32 +74,114 @@
 
 
 // THE VERSION OF USING MAPBOX TP DETECT MY LOCATION
+//import SwiftUI
+//import MapboxMaps
+//
+//struct MapViewContainer: UIViewRepresentable {
+//    let accessToken = "sk.eyJ1IjoiZ3JhY2Vzb2wiLCJhIjoiY2xteng2eWF0MW5zdjJqbnRrN3dlNXVkaiJ9.xYpQiPGGe01KVlnx9Qf0_w"
+//    @Binding var userLocation: CLLocationCoordinate2D?
+//
+//    func makeUIView(context: Context) -> MapView {
+//        let resourceOptions = ResourceOptions(accessToken: accessToken)
+//        let mapInitOptions = MapInitOptions(resourceOptions: resourceOptions)
+//        let mapView = MapView(frame: UIScreen.main.bounds, mapInitOptions: mapInitOptions)
+//
+//        // Enable user location tracking
+//        mapView.location.options.puckType = .puck2D()
+//
+//        return mapView
+//    }
+//
+//    func updateUIView(_ uiView: MapView, context: Context) {
+//        // Check if we have the user's location available
+//        if let userLocation = userLocation {
+//            // Use the user's location as the center coordinate for the map
+//            let cameraOptions = CameraOptions(center: userLocation, zoom: 15)
+//            uiView.camera.ease(to: cameraOptions, duration: 1.0) { _ in
+//                // Completion handler, if needed
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+// THE VERSION OF USING MAPBOX TP DETECT MY LOCATION
 import SwiftUI
 import MapboxMaps
+import MapboxCommon
 
 struct MapViewContainer: UIViewRepresentable {
     let accessToken = "sk.eyJ1IjoiZ3JhY2Vzb2wiLCJhIjoiY2xteng2eWF0MW5zdjJqbnRrN3dlNXVkaiJ9.xYpQiPGGe01KVlnx9Qf0_w"
     @Binding var userLocation: CLLocationCoordinate2D?
-
-    func makeUIView(context: Context) -> MapView {
-        let resourceOptions = ResourceOptions(accessToken: accessToken)
-        let mapInitOptions = MapInitOptions(resourceOptions: resourceOptions)
-        let mapView = MapView(frame: UIScreen.main.bounds, mapInitOptions: mapInitOptions)
+    var events: [EventData]
+    @State private var mapView: MapView?
+    
+    func makeUIView(context: Context) -> MapboxMaps.MapView {
+        let mapView = MapboxMaps.MapView(frame: CGRect.zero, styleURL: nil)
+        mapView.delegate = context.coordinator
+        self.mapView = mapView // Store a reference to the MapView
 
         // Enable user location tracking
-        mapView.location.options.puckType = .puck2D()
+        mapView.showsUserLocation = true
+
+        // Set the initial center coordinate and zoom level
+        if let userLocation = userLocation {
+            let cameraOptions = CameraOptions(center: userLocation, zoom: 7)
+            mapView.camera.ease(to: cameraOptions, duration: 1.0)
+        }
 
         return mapView
     }
 
-    func updateUIView(_ uiView: MapView, context: Context) {
-        // Check if we have the user's location available
+    func updateUIView(_ uiView: MapboxMaps.MapView, context: Context) {
+        // Update the MapView when the user's location changes
         if let userLocation = userLocation {
-            // Use the user's location as the center coordinate for the map
-            let cameraOptions = CameraOptions(center: userLocation, zoom: 15)
-            uiView.camera.ease(to: cameraOptions, duration: 1.0) { _ in
-                // Completion handler, if needed
+            let cameraOptions = CameraOptions(center: userLocation, zoom: uiView.cameraState.zoom)
+            uiView.camera.ease(to: cameraOptions, duration: 1.0)
+        }
+
+        // Clear existing annotations and add new ones from events
+        uiView.removeAnnotations(uiView.annotations ?? [])
+        addAnnotationsToMapView(uiView)
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MGLMapViewDelegate {
+        var parent: MapViewContainer
+
+        init(_ parent: MapViewContainer) {
+            self.parent = parent
+        }
+
+        // Implement MGLMapViewDelegate methods if needed
+    }
+
+    private func addAnnotationsToMapView(_ mapView: MapboxMaps.MapView) {
+        var markers: [MarkerAnnotation] = []
+        
+        for event in events {
+            if let latitude = event.venue?.latitude, let longitude = event.venue?.longitude {
+                // Create a marker annotation
+                let marker = MarkerAnnotation(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+                
+                // Customize the marker with text and icon
+                let textElement = Element.text(text: .formatted(.value(event.name)))
+                let iconElement = Element.icon(.image(.constant(UIImage(named: "red-pin-image")!)))
+                
+                marker.element = .composite(elements: [textElement, iconElement])
+                
+                markers.append(marker)
             }
         }
+        
+        // Add the marker annotations to the map
+        mapView.annotations.annotations = markers
     }
+
 }
