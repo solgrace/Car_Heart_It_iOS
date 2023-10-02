68df25fbc84724047463f229b85c990191e29ce0
solgrace <solibungrace@gmail.com>
2023-10-02 18:58:47 +1100

    Improved event details display for better readability, and added a '< Back' navigation button to return to EventMapView in EachEventView.

--- Event_Heart_It_iOS/View/EachEventView.swift
+++ Event_Heart_It_iOS/View/EachEventView.swift
@@ -8,117 +8,138 @@
 import SwiftUI
 
 struct EachEventView: View {
+    @Environment(\.presentationMode) var presentationMode
     let event: EventData
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 16) {
-                
-                // Add spacing at the top
-                Spacer()
-                    .frame(height: 40)
-                
-                // Event Name
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Event Name:")
-                        .font(.body)
-                        .foregroundColor(.gray)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
 
-                    Text("\(event.name)")
-                        .font(.title)
-                }
-                .padding()
-
-                // Event Description
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Description:")
-                        .font(.headline)
-                    
-                    Text("\(event.description ?? "N/A")")
-                        .font(.body)
-                }
-                .padding()
-                                                
-                // Event Is Virtual
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Is Virtual:")
-                        .font(.headline)
-                    
-                    Text("\(event.is_virtual.map(String.init(describing:)) ?? "N/A")")
-                        .font(.body)
-                }
-                .padding()
-                
-                // Event Start Time
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Start Time:")
-                        .font(.headline)
-                    
-                    Text("\(event.start_time ?? "N/A")")
-                        .font(.body)
-                }
-                .padding()
-                
-                // Event End Time
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("End Time:")
-                        .font(.headline)
-                    
-                    Text("\(event.end_time ?? "N/A")")
-                        .font(.body)
-                }
-                .padding()
-                                
-                // Event Link
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Link:")
-                        .font(.headline)
-                    
-                    Text("\(event.link ?? "N/A")")
-                        .font(.body)
-                }
-                .padding()
-
-                // Event Venue
-                if let venue = event.venue {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Venue:")
-                            .font(.title2)
-
-                        // Venue Subtypes
-                        if let subtypes = venue.subtypes, !subtypes.isEmpty {
-                            let subtypesString = subtypes.joined(separator: ", ")
-                            Text("Subtypes: \(subtypesString)")
-                                .font(.headline)
-                        } else {
-                            Text("Subtypes: N/A")
-                                .font(.body)
-                        }
+                    // Event Name
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Event Name:")
+                            .font(.body)
+                            .foregroundColor(.gray)
                         
-                        // Venue Address
-                        VStack(alignment: .leading, spacing: 4) {
+                        Text("\(event.name)")
+                            .font(.title)
+                    }
+                    .padding()
+                    
+                    // Event Description
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Description:")
+                            .font(.headline)
+                        
+                        Text("\(event.description ?? "N/A")")
+                            .font(.body)
+                    }
+                    .padding()
+                    
+                    // Event Is Virtual
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Is Virtual:")
+                            .font(.headline)
+                        
+                        Text("\(event.is_virtual.map(String.init(describing:)) ?? "N/A")")
+                            .font(.body)
+                    }
+                    .padding()
+                    
+                    // Event Start Time
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Start Time:")
+                            .font(.headline)
+                        
+                        Text("\(event.start_time ?? "N/A")")
+                            .font(.body)
+                    }
+                    .padding()
+                    
+                    // Event End Time
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("End Time:")
+                            .font(.headline)
+                        
+                        Text("\(event.end_time ?? "N/A")")
+                            .font(.body)
+                    }
+                    .padding()
+                    
+                    // Event Link
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Link:")
+                            .font(.headline)
+                        
+                        Text("\(event.link ?? "N/A")")
+                            .font(.body)
+                    }
+                    .padding()
+                    
+                    // Event Venue
+                    if let venue = event.venue {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Venue:")
+                                .font(.title2)
+                                .bold()
+                            
+                            Spacer()
+                                .frame(height: 10)
+                            
+                            // Venue Subtypes
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text("Subtypes:")
+                                    .font(.headline)
+                                
+                                if let subtypes = venue.subtypes, !subtypes.isEmpty {
+                                    let subtypesString = subtypes.joined(separator: ", ")
+                                    Text(subtypesString)
+                                        .font(.body)
+                                        .padding(.bottom, 8) // Move the bottom padding here
+                                } else {
+                                    Text("N/A")
+                                        .font(.body)
+                                        .padding(.bottom, 8) // Move the bottom padding here
+                                }
+                                
+                                Spacer()
+                                    .frame(height: 20)
+                            }
+
+                            
+                            // Venue Address
                             Text("Address:")
                                 .font(.headline)
                             
                             Text("\(venue.full_address ?? "N/A")")
                                 .font(.body)
+                            
+                            
+                            // Add more venue details if needed
                         }
                         .padding()
-
-                        // Add more venue details if needed
+                    } else {
+                        Text("Venue: N/A")
+                            .font(.body)
+                            .padding()
                     }
-                    .padding()
-                } else {
-                    Text("Venue: N/A")
-                        .font(.body)
-                        .padding()
+                    
+                    // Add more sections for other event details
+                    
                 }
-
-                // Add more sections for other event details
-
+//                .padding()
+                .padding([.horizontal, .bottom], 15) // Adds horizontal (left and right) and bottom padding
+                .edgesIgnoringSafeArea(.all) // Ignore safe area to fill the entire screen
             }
-            .padding()
+            // Remove title if "< Back" not showing ↓
+            .navigationBarTitle("Event Details", displayMode: .inline)
+            // Remove title if "< Back" not showing ↑
+            
+            .navigationBarItems(leading: Button("< Back") {
+                // This action will pop the view off the navigation stack
+                presentationMode.wrappedValue.dismiss()
+            })
         }
-        .navigationBarTitle("Event Details", displayMode: .inline)
     }
 }
