cdcc694230959351ed29253e87ca9ee944071d35
solgrace <solibungrace@gmail.com>
2023-10-07 08:07:38 +1100

    Successfully added Firebase into my project.

--- Event_Heart_It_iOS/Event_Heart_It_iOSApp.swift
+++ Event_Heart_It_iOS/Event_Heart_It_iOSApp.swift
@@ -6,10 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Event_Heart_It_iOSApp: App {
     let persistenceController = PersistenceController.shared
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
