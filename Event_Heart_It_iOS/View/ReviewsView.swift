9a1f434b1029e728cea74047c5be374215167762
solgrace <solibungrace@gmail.com>
2023-10-09 18:12:53 +1100

    Fixed a problem where entering a review for one event affected all events' review text due to a shared state. Now, each event has its own separate review text, ensuring that user input for one event is independent and isolated from other event's TextField (in other words, ensuring that the text entered in one event's TextField, this same text does not render in the other event's TextField).

--- Event_Heart_It_iOS/View/ReviewsView.swift
+++ Event_Heart_It_iOS/View/ReviewsView.swift
@@ -5,6 +5,111 @@
 //  Created by Grace Rufina Solibun on 2/10/2023.
 //
 
+//import SwiftUI
+//
+//struct ReviewsView: View {
+//
+//    @Environment(\.presentationMode) var presentationMode
+//    @StateObject private var bookedEventsViewModel = BookedEventsViewModel(coreDataManager: CoreDataManager.shared)
+//    @StateObject private var reviewsViewModel = ReviewsViewModel(persistentContainer: CoreDataManager.shared.persistentContainer)
+//    @State private var reviewText: String = ""
+//    @State private var selectedEvent: BookedEventStruct?
+//
+////    @State private var selectedEvent: BookedEvent?
+////    @State private var selectedEvent: BookedEventStruct? // Adjust the type
+//
+//    var body: some View {
+//        List {
+//            ForEach(bookedEventsViewModel.bookedEvents, id: \.id) { bookedEvent in
+//                Section(header: Text("\(bookedEvent.eventName)")
+//                    .font(.title2)
+//                    .fontWeight(.bold)
+//                    .foregroundColor(.black)
+//                ) {
+//                    VStack(alignment: .leading, spacing: 10) {
+//                        Text("Start Time:")
+//                            .font(.body)
+//                            .foregroundColor(.gray)
+//                        Text(formatDate(bookedEvent.eventStartDate))
+//                            .padding(.vertical, 5)
+//
+//                        Text("End Time:")
+//                            .font(.body)
+//                            .foregroundColor(.gray)
+//                        Text(formatDate(bookedEvent.eventEndDate))
+//                            .padding(.vertical, 5)
+//
+//                        Text("Full Address:")
+//                            .font(.body)
+//                            .foregroundColor(.gray)
+//                        Text(bookedEvent.eventFullAddress)
+//                            .padding(.vertical, 5)
+//
+//                        TextField("Leave a review...", text: $reviewText)
+//                            .padding(.vertical, 10)
+//                            .textFieldStyle(RoundedBorderTextFieldStyle())
+//
+//                        Button("Submit Review") {
+//                            // Use the bookedEvent directly without setting selectedEvent
+//                            reviewsViewModel.submitReview(bookedEvent: bookedEvent, reviewText: reviewText) {
+//                                // Reload reviews for the current event after submitting
+//                                reviewsViewModel.fetchUniqueReviewsForEventFromCoreData(eventID: bookedEvent.eventID) { reviews in
+//                                    // Handle the fetched reviews
+//                                    DispatchQueue.main.async {
+//                                        reviewsViewModel.displayedReviews = reviews
+//                                    }
+//                                }
+//                            }
+//                        }
+//
+//                        // Display reviews for the current event
+//                        ForEach(reviewsViewModel.displayedReviews.filter { $0.eventID == bookedEvent.eventID }, id: \.id) { review in
+//                            Text("\((review.reviewerFirstName ?? "") + (review.reviewerLastName ?? "")): \(review.reviewText ?? "")")
+//                                .padding(.vertical, 5)
+//                        }
+//                    }
+//                }
+//                .padding(.vertical, 18)
+//            }
+//            .listRowSeparator(.hidden)
+//        }
+//        .onAppear {
+//            // Fetch past booked events when the view appears
+//            bookedEventsViewModel.getBookedEvents {
+//                // This is the completion handler, and you can do something if needed
+//            }
+//
+//            // Fetch all reviews when the view appears
+//            reviewsViewModel.fetchAllReviewsFromCoreData { reviews in
+//                // Handle the fetched reviews
+//                DispatchQueue.main.async {
+//                    reviewsViewModel.displayedReviews = reviews
+//                }
+//            }
+//        }
+//        .navigationBarItems(leading: Button("Back") {
+//            // Dismiss this view and navigate back to the previous view
+//            presentationMode.wrappedValue.dismiss()
+//        })
+//
+//        .navigationBarTitle("", displayMode: .inline)
+//        .navigationBarBackButtonHidden(true)
+//        .navigationBarHidden(true)
+//    }
+//
+//    func formatDate(_ timeInterval: TimeInterval) -> String {
+//        let date = Date(timeIntervalSince1970: timeInterval)
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "MMM dd, yyyy hh:mm a"
+//        return formatter.string(from: date)
+//    }
+//
+//}
+
+
+
+
+
 import SwiftUI
 
 struct ReviewsView: View {
@@ -17,6 +122,21 @@ struct ReviewsView: View {
     
 //    @State private var selectedEvent: BookedEvent?
 //    @State private var selectedEvent: BookedEventStruct? // Adjust the type
+    
+    
+    
+    
+    
+    // Dictionary to store reviewText for each event
+    @State private var reviewTexts: [UUID: String] = [:]
+
+    // Helper function to get a Binding for reviewText based on the event
+    private func getBinding(for event: BookedEventStruct) -> Binding<String> {
+        return Binding(
+            get: { self.reviewTexts[event.id] ?? "" },
+            set: { self.reviewTexts[event.id] = $0 }
+        )
+    }
 
     var body: some View {
         List {
@@ -45,13 +165,13 @@ struct ReviewsView: View {
                         Text(bookedEvent.eventFullAddress)
                             .padding(.vertical, 5)
                         
-                        TextField("Leave a review...", text: $reviewText)
+                        TextField("Leave a review...", text: getBinding(for: bookedEvent))
                             .padding(.vertical, 10)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
-
+                        
                         Button("Submit Review") {
                             // Use the bookedEvent directly without setting selectedEvent
-                            reviewsViewModel.submitReview(bookedEvent: bookedEvent, reviewText: reviewText) {
+                            reviewsViewModel.submitReview(bookedEvent: bookedEvent, reviewText: reviewTexts[bookedEvent.id] ?? "") {
                                 // Reload reviews for the current event after submitting
                                 reviewsViewModel.fetchUniqueReviewsForEventFromCoreData(eventID: bookedEvent.eventID) { reviews in
                                     // Handle the fetched reviews
